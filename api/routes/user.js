ac7ea8716c6e136e0bdcaec227b6ca920972762d
Mahamud Arteh <artehrush@gmail.com>
2022-01-15 11:51:10 +0000

    user update introduced

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,3 +1,5 @@
+const { verifyToken } = require('./verifyToken');
+
 const userRouter = require('express').Router();
 
 userRouter.get('/usertest', (req, res) => {
@@ -7,6 +9,28 @@ userRouter.get('/usertest', (req, res) => {
 userRouter.post('/userposttest', (req, res) => {
 	const username = req.bodu.username;
 	res.status(200).send('your username is:' + username);
+});
+
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+	if (req.body.password) {
+		req.body.password = CryptoJS.AES.encrypt(
+			req.body.password,
+			process.env.PASS_SEC
+		).toString();
+	}
+
+	try {
+		const updatedUser = await User.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: req.body,
+			},
+			{ new: true }
+		);
+		res.status(200).json(updatedUser);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = userRouter;
