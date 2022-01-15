1b133740ae5fb5391fa26ca8ad9815cec1368d3f
Mahamud Arteh <artehrush@gmail.com>
2022-01-15 13:42:22 +0000

    cart retrieve code

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -37,10 +37,20 @@ router.put('/:id', async (req, res) => {
 });
 
 //DELETE
-router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+router.delete('/:id', async (req, res) => {
 	try {
 		await Cart.findByIdAndDelete(req.params.id);
 		res.status(200).json('Cart has been deleted...');
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+//GET USER CART
+router.get('/find/:userId', async (req, res) => {
+	try {
+		const cart = await Cart.findOne({ userId: req.params.userId });
+		res.status(200).json(cart);
 	} catch (err) {
 		res.status(500).json(err);
 	}
