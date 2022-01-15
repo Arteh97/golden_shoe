bfeb319db6a0c0ea02fc75c05e37e3282636943e
Mahamud Arteh <artehrush@gmail.com>
2022-01-15 00:17:08 +0000

    database connection succesful

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://Arteh:2BothamClose!@cluster0.d5une.mongodb.net/Shop?retryWrites=true&w=majority")
+mongoose
+	.connect(
+		'mongodb+srv://Arteh:2BothamClose!@golden-shoe.d5une.mongodb.net/golden-shoe?retryWrites=true&w=majority'
+	)
+	.then(() => console.log('Database connection successful'));
 
-
-app.listen(5000, () => {
-    console.log("Backend server is running!")
-}).then(() => console.log("Database connection successful"));
+app.listen(5001, () => {
+	console.log('Backend server is running!');
+});
